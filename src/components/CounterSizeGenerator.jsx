f1903f0d51ccd4aa4f4a7f8eafaa18b49a1a310f
Tom Lo <loto2@oocl.com>
2020-12-07 22:09:13 +0800

    [counter][tom] feat: add CounterSizeGenerator to capture user input

---
+++ src/components/CounterSizeGenerator.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class CounterSizeGenerator extends Component {
+  handleChange = (event) => {
+    this.props.setSize(event.target.value);
+  }
+
+  render() {
+    const { size } = this.props;
+
+    return (
+      <div>
+        <input type="number" value={size} onChange={this.handleChange} />
+      </div>
+    )
+  }
+}
+
+export default CounterSizeGenerator;
